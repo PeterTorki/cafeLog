2402e777ad53928ecf552aea6c6aa351bc3312fb
Peter_Torki <peter.j.torki@gmail.com>
2023-08-21 02:46:57 +0300

    Not yet

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,52 +1,48 @@
-import React, { useEffect, useState, useCallback } from "react";
-import axios from "axios";
-import TypesMenu from "./TypesMenu";
-import styles from "../Style/Menu.module.css";
+import React, {useEffect, useState, useCallback} from 'react'
+import axios from 'axios'
+import TypesMenu from './TypesMenu';
 
 const Menu = () => {
-  const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState([]);
   const [currType, setCurrType] = useState("All Menu");
 
   const getProducts = useCallback(() => {
-    axios
-      .get("http://localhost:3477/Products")
-      .then((response) => {
-        setProducts(response.data);
-      })
-      .catch((e) => console.log(e));
-  }, [products]);
+		axios.get('http://localhost:3477/Products').then((response) => {
+      setProducts(response.data);
+    })
+	}, [products])
+
+
+	useEffect(() => {
+		getProducts();
+	}, []);
+
 
   const handleCurrTypeState = (type) => {
     setCurrType(type);
   };
 
-  useEffect(() => {
-    getProducts();
-  }, []);
-
-  const currentElemetns = products.filter((p) => {
+  const currentElements = products.filter((p) => {
     if (currType === "All Menu") return p;
     if (currType === p.type) return p;
   });
-  const divDisplay = currentElemetns.map((p) => {
+  const divDisplay = currentElements.map((p) => {
     return <div key={p.id}></div>;
   });
 
-  // console.log(currentElemetns);
-  const p = products[0];
+  const p = products[9];
   console.log(p);
-  console.log(products);
   return (
     <div>
       <TypesMenu typeSetState={handleCurrTypeState} />
       <div>
-        <div className={styles.card}>
-          <div className={styles.info}>
+        <div>
+          <div>
             <img src="" />
-            <div className={styles.textInfo}>
-              <h5>{p.name}</h5>
-              <p>{p.description}</p>
-              <h4>{p.price}$</h4>
+            <div>
+              <h5>{p?.name}</h5>
+              <p>{p?.description}</p>
+              <h4>{p?.price}$</h4>
             </div>
           </div>
         </div>
@@ -55,4 +51,4 @@ const Menu = () => {
   );
 };
 
-export default Menu;
+export default Menu
