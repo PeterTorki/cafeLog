6167b1d894a9ae5699b7273c9acce2bc13bbab89
Peter_Torki <peter.j.torki@gmail.com>
2023-08-21 20:14:22 +0300

    Last Update

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -15,33 +15,33 @@ const SideBar= () =>{
     return (    
         <ul className={show? styles.sidebar2 : styles.sidebar }>
             <div className={styles.main} onClick={()=> setShow(!show)}>
-                <GiHamburgerMenu className={styles.icon}/>
+                <GiHamburgerMenu className={styles.icon} color={!show? '#FFFFFF': "000000"} size={30}/>
             </div>
-            <li >
-                <GoHomeFill className={styles.icon}/>
+            <li>
+                <GoHomeFill color={!show? '#FFFFFF': "000000"} size={30}/>
                 {show? "" :<h2>Home</h2>}
             </li>
-            <li >
-                <AiOutlineCoffee className={styles.icon}/>
+            <li>
+                <AiOutlineCoffee className={styles.icon} color={!show? '#FFFFFF': "000000"} size={30}/>
                 {show? "" :<h2>Menu</h2>}
             </li>
-            <li >
-                <AiOutlineHeart className={styles.icon}/>
-                {show? "" :<h2>Favourites</h2>}
+            <li>
+                <AiOutlineHeart color={!show? '#FFFFFF': "000000"} size={30}/>
+                {show? "" :<h2>Favorites</h2>}
             </li>
             <li>
-                <AiOutlineShoppingCart className={styles.icon}/>
+                <AiOutlineShoppingCart color={!show? '#FFFFFF': "000000"} size={30}/>
                 {show? "" :<h2>Cart</h2>}
             </li>
-            <li >
-                <AiOutlineQuestionCircle className={styles.icon}/>
+            <li>
+                <AiOutlineQuestionCircle className={styles.icon} color={!show? '#FFFFFF': "000000"} size={30}/>
                 {show? "" :<h2>About</h2>}
             </li>
-            <li >
-                <AiOutlinePhone className={styles.icon}/>
+            <li>
+                <AiOutlinePhone className={styles.icon} color={!show? '#FFFFFF': "000000"} size={30}/>
                 {show? "" :<h2>Contact</h2>}
             </li> 
-            <li >
+            <li>
                 <BsFillGearFill className={styles.iconS}/>
                 {show? "" :<h2 className={styles.sett}>Settting</h2>}
             </li>
