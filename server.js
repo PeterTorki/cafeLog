db78513b2d45a0330a1fac1a4440b8dbb14004fb
Peter_Torki <peter.j.torki@gmail.com>
2023-08-26 04:53:23 +0300

    Remove Proc

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./data.json');
 
-const middlewares = jsonServer.defaults({
-		static: './build',
-})
+const middlewares = jsonServer.defaults()
 
 const port = process.env.PORT || 3466;
 server.use(middlewares);
