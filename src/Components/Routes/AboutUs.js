155b6c2393329d5fc18b6b7d341926b2aaf3d85f
Peter_Torki <peter.j.torki@gmail.com>
2023-09-02 16:05:04 +0300

    About

--- src/Components/Routes/AboutUs.js
+++ src/Components/Routes/AboutUs.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import about from "../imgsComponent/about.png"
 
-const AboutUs = props => {
-	return (
-		<div>AboutUs</div>
-	)
+export default function AboutUs() {
+  return (
+    <section>
+      <div class="container">
+        <div class="row  align-content-center p-5">
+          <div class="col-lg-5 col-12 p-2">
+            <img class="img-fluid"  src={about}  alt="..." />
+          </div>
+          <div className="col-lg-2 col-1">
+
+          </div>
+          <div class="col-lg-5 col-12 p-2">
+            <h2>About us</h2>
+            <p>
+            Opening a Coffee Shop is challenging work. It can certainly sound like a novel idea.
+            We created an online Coffee Shop "ChillCup" to help people order using delivery
+            ,They have a full managable User System: Login, Sign Up
+            ,Add and Remove To Cart and also have favorites Section.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
-
-
-export default AboutUs
